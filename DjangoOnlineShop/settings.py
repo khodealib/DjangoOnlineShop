a779d0707c8b42e49b3c0d8c947995a37e3c7659
Ali Bagheri <khodelaib@gmail.com>
2023-11-14 08:32:42 +0330

    use postgresql.

--- DjangoOnlineShop/settings.py
+++ DjangoOnlineShop/settings.py
@@ -106,9 +106,13 @@ WSGI_APPLICATION = 'DjangoOnlineShop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_shop",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
