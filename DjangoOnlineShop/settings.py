fe8b8247199fe4979ec78e3972b5b286fc20c5e8
Ali Bagheri <khodelaib@gmail.com>
2024-01-25 10:43:26 +0330

    delete decouple dependency.

--- DjangoOnlineShop/settings.py
+++ DjangoOnlineShop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 
-from decouple import config
 from django.utils import timezone
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', 'django-insecure-3zz^@6himy_%g1l5(ooqnmm@cml2ocyehiob#6f53&!0bjil3y')
+SECRET_KEY = 'django-insecure-3zz^@6himy_%g1l5(ooqnmm@cml2ocyehiob#6f53&!0bjil3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
